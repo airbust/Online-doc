4dfe9f49c111a558f713eb2b400662712dad60db
qxforever <53657439+qxforever@users.noreply.github.com>
2020-08-14 00:38:09 +0800

    增/删/改功能的基本实现

--- back/src/main/java/com/example/service/FileService.java
+++ back/src/main/java/com/example/service/FileService.java
@@ -23,79 +23,130 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class FileService {
-    @Autowired
-    private UserDao userDao;
-    @Autowired
-    private GroupDao groupDao;
-    @Autowired
-    private FileDao fileDao;
-    @Autowired
-    private JwtConfig jwtConfig;
-    @Autowired
-    private HttpServletRequest request;
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
+	@Autowired
+	private UserDao userDao;
+	@Autowired
+	private GroupDao groupDao;
+	@Autowired
+	private FileDao fileDao;
+	@Autowired
+	private JwtConfig jwtConfig;
+	@Autowired
+	private HttpServletRequest request;
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
 
-    public Map<String, Object> findFileById(Integer fileId) throws RuntimeException {
-        //TODO 前提用户已登录
-        String userName = jwtTokenUtil.getUsernameFromRequest(request);
+	public File findFileById(Integer fileId) throws RuntimeException {
+		// TODO 权限
+		File file = fileDao.getFileById(fileId);
+		if(file == null)
+			throw new RuntimeException("文件不存在");
+		return file;
+	}
 
-        //签发token
-        final UserDetails userDetails = this.loadUserByUsername(Integer.toString(fileId));
-        final String token = jwtTokenUtil.generateToken(userDetails);
-        //获取用户权限
-        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
-        List<String> roles = new ArrayList<>();
-        for (GrantedAuthority authority : authorities) {
-            roles.add(authority.getAuthority());
-        }
-        Map<String, Object> map = new HashMap<>(3);
-        map.put("token", jwtConfig.getPrefix() + token);
-        map.put("name", userName);
-        map.put("roles", roles);
+	// ******************************************************************************
 
-        //设置过期时间 (key value time s)
-        redisTemplate.opsForValue().
-                set("TOKEN_" + userName, jwtConfig.getPrefix() + token, 60, TimeUnit.SECONDS);
-        return map;
-    }
+	public void newFile(String fileName, String fileBody) throws RuntimeException {
+		if (fileName.equals(""))
+			throw new RuntimeException("标题为空");
+		User user = userDao.getUserByName(jwtTokenUtil.getUsernameFromRequest(request));
+		File file = new File(fileName, fileBody, new Date(), user.getId(), 0);
+		fileDao.saveFile(file);
+	}
 
-    //******************************************************************************
+	public void editFile(Integer fileId,String fileName,String fileBody) throws RuntimeException {
+		
+		File file = fileDao.getFileById((fileId));
+		if(file == null) 
+			throw new RuntimeException("文件不存在");
+		file.setFileName(fileName);
+		file.setFileBody(fileBody);
+		file.setModifyTime(new Date());
+		file.setModifyCnt(file.getModifyCnt() + 1);
+		fileDao.saveFile(file);
+	}
 
-    /**
-     * 通过用户名加载用户到 Spring Security
-     * @param fileId 文档id
-     * @return 用户信息
-     */
-    public UserDetails loadUserByUsername(String fileId) throws UsernameNotFoundException {
-        User user = userDao.getUserByName(jwtTokenUtil.getUsernameFromRequest(request));
-        Integer userId = user.getId();
-        List<SimpleGrantedAuthority> authorities = new ArrayList<>(1);
-        //用于添加用户的权限。将用户权限添加到authorities
-        File file = fileDao.getFileById(Integer.valueOf(fileId));
-        Role role = new Role();
-        if(file.getUserId()!=0){ // 个人文档
-            if(file.getUserId().equals(userId)) role.setName("USER");
-            else role.setName("OTHER");
-        }
-        else{ // 团队文档
-            Group group = groupDao.getGroupById(file.getGroupId());
-            if(group.getAdminId().equals(userId)) role.setName("USER");
-            else{
-                List<User> members = groupDao.getMemberById(file.getGroupId());
-                boolean flag = true;
-                for(User member : members){
-                    if(member.getId().equals(userId)) {
-                        role.setName("GROUP"); flag=false;
-                    }
-                }
-                if(flag) role.setName("OTHER");
-            }
-        }
-        authorities.add(new SimpleGrantedAuthority(role.getName()));
-        return new org.springframework.security.core.userdetails.User(user.getName(), "***********", authorities);
-    }
+	public void deleteFile(Integer fileId) throws RuntimeException {
+		File file = fileDao.getFileById(fileId);
+		if(file == null)
+			throw new RuntimeException("文件不存在");
+		fileDao.deleteFile(fileId);
+	}
+	
+	public void recoverFile(Integer fileId) throws RuntimeException {
+		File file = fileDao.getFileById(fileId);
+		if(file == null)
+			throw new RuntimeException("文件未删除");
+		if(fileId > File.fileCnt || fileId <= 0)
+			throw new RuntimeException("文件不存在");
+		fileDao.recoverFile(fileId);
+	}
+	
+	public void updateFileState(Integer fileId) throws RuntimeException {
+		File file = fileDao.getFileById(fileId);
+		if(file == null)
+			throw new RuntimeException("文件不存在");
+		fileDao.updateEditState(fileId);
+	}
+	
+	public boolean isEditable(Integer fileId) throws RuntimeException{
+		File file = fileDao.getFileById(fileId);
+		if(file == null)
+			throw new RuntimeException("文件不存在");
+		return file.getIsEdit() == 0;
+	}
+	
+	public List<File> getDeletedFile() {
+		User user = userDao.getUserByName(jwtTokenUtil.getUsernameFromRequest(request));
+		return fileDao.getDeletedFileByUserId(user.getId());
+	}
+	
+	public List<File> getCreationFile() {
+		User user = userDao.getUserByName(jwtTokenUtil.getUsernameFromRequest(request));
+		return fileDao.getFileByUserId(user.getId());
+	}
+	
+	/**
+	 * 通过用户名加载用户到 Spring Security
+	 * 
+	 * @param fileId 文档id
+	 * @return 用户信息
+	 */
+	public UserDetails loadUserByUsername(String fileId) throws UsernameNotFoundException {
+		User user = userDao.getUserByName(jwtTokenUtil.getUsernameFromRequest(request));
+		Integer userId = user.getId();
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>(1);
+		// 用于添加用户的权限。将用户权限添加到authorities
+		File file = fileDao.getFileById(Integer.valueOf(fileId));
+		Role role = new Role();
+		if (file.getUserId() != 0) { // 个人文档
+			if (file.getUserId().equals(userId))
+				role.setName("USER");
+			else
+				role.setName("OTHER");
+		} else { // 团队文档
+			Group group = groupDao.getGroupById(file.getGroupId());
+			if (group.getAdminId().equals(userId))
+				role.setName("USER");
+			else {
+				List<User> members = groupDao.getMemberById(file.getGroupId());
+				boolean flag = true;
+				for (User member : members) {
+					if (member.getId().equals(userId)) {
+						role.setName("GROUP");
+						flag = false;
+					}
+				}
+				if (flag)
+					role.setName("OTHER");
+			}
+		}
+		authorities.add(new SimpleGrantedAuthority(role.getName()));
+		return new org.springframework.security.core.userdetails.User(user.getName(), "***********", authorities);
+	}
+	
+	
 
 }
