9033e2499fb54a5527033b20c8cdbe5245c88329
qlylangyu <1252536415@qq.com>
2020-08-14 17:52:28 +0800

    继承BaseMapper

--- back/src/main/java/com/example/dao/GroupDao.java
+++ back/src/main/java/com/example/dao/GroupDao.java
@@ -2,6 +2,7 @@ package com.example.dao;
 
 import com.example.entity.Group;
 import com.example.entity.User;
+import com.example.utils.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Repository
 @Mapper
-public interface GroupDao {
+public interface GroupDao extends BaseMapper<Group> {
 
     Group getGroupById(Integer groupId);
     List<User> getMemberById(Integer groupId);
